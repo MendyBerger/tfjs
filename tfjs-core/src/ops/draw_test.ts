eb68a64a8f3f17489800b88879f57e1d2d1244ab
Xu Xing <xing.xu@intel.com>
2023-05-09 13:54:45 +0800

    [webgpu] Implement draw with graphics draw

--- tfjs-core/src/ops/draw_test.ts
+++ tfjs-core/src/ops/draw_test.ts
@@ -16,7 +16,7 @@
  */
 
 import * as tf from '../index';
-import {BROWSER_ENVS, describeWithFlags} from '../jasmine_util';
+import {Constraints, describeWithFlags} from '../jasmine_util';
 import {expectArraysClose, expectArraysEqual} from '../test_util';
 
 class MockContext {
@@ -44,7 +44,12 @@ class MockCanvas {
   }
 }
 
-describeWithFlags('Draw on 2d context', BROWSER_ENVS, () => {
+const BROWSER_NO_WEBGPU_ENVS: Constraints = {
+  predicate: (env) =>
+      (env.backendName !== 'webgpu' && tf.env().platformName === 'browser')
+};
+
+describeWithFlags('Draw on 2d context', BROWSER_NO_WEBGPU_ENVS, () => {
   it('draw image with 4 channels and int values', async () => {
     const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
     const img = tf.tensor3d(data, [2, 2, 4], 'int32');
